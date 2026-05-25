7a3e3837ea90d2636719dbb325dae579912f68c1
ashishsahoo18 <sahooashish673@gmail.com>
2026-05-24 21:40:41 -0500

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -2,3 +2,9 @@
 # Print numbers from 10 down to 1, one per line.
 for i in range(10,0,-1):
     print(i)
+
+# 2. Print each item in a list
+# Given fruits = ["apple", "banana", "cherry"], print each fruit on its own line.
+fruits = ["apple","banana","cherry"]
+for fruit in (fruits):
+    print(fruit)
