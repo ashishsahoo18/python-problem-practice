4dc1e92f3d606a8c4db835a81d0c530b67de4f45
ashishsahoo18 <sahooashish673@gmail.com>
2026-05-27 22:50:04 -0500

    Create Day3.py

---
+++ Day3.py
@@ -0,0 +1,12 @@
+# 1.Find the sum of all even numbers from 1 to 50
+total = 0
+for i in range (1,51):
+    if i%2 == 0:
+        total += i
+        print("sum =",total)
+
+# 2.Print the square of numbers from 1 to 10.
+total = 0
+for i in range (1,11):
+    total += i
+    print("Square of ","=",i*i)
