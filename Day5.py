de56652a6bbe1feb500cd95d73b3fbf6941fe3d7
ashishsahoo18 <sahooashish673@gmail.com>
2026-05-29 13:07:01 -0500

    Update Day5.py

--- Day5.py
+++ Day5.py
@@ -1,10 +1,11 @@
 # 1. Count digits in a number
 # A.using string
-num = int(input("Enter number:"))
-print("Total digits:",len(num))
+num = input("Enter a number:")
+
+print("Total digits:", len(num))
 
 # B.
-num = int(input("Enter number:"))
+num = int(input("Enter a number:"))
 count = 0
 while num != 0:
     num = num//10
@@ -12,7 +13,7 @@ while num != 0:
 print("Total digits:",count)
 
 # C.Code negative numbers aur 0 ko properly handle karke digits count karta hai.
-num = abs(int(input("Enter number:")))
+num = abs(int(input("Enter a number:")))
 count = 0
 
 if num ==0:
