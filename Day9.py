79ac323af9c86f38fac8c88d8d7dbc53c9ed3601
ashishsahoo18 <sahooashish673@gmail.com>
2026-06-02 10:45:40 -0500

    Update Day9.py

--- Day9.py
+++ Day9.py
@@ -11,3 +11,10 @@ while guess != secret_number:
         print("Too high")
     else:
         print("Congratulation! You correctly Guessed.")
+
+# 2. Print all tables from 1–10 using nested loops.
+for i in range(1,11):
+    print(f"\n Table of {i}")
+
+    for j in range(1,11):
+        print(f"\n {i}*{j}={i*j}",end ="")
