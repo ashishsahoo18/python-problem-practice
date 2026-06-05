c9ce43901902fd9f02d247c8c5c2b410eb96cea4
ashishsahoo18 <sahooashish673@gmail.com>
2026-06-05 10:52:18 -0500

    Create Day11.py

---
+++ Day11.py
@@ -0,0 +1,16 @@
+# 1. Number pyramid
+# 1
+# 121
+# 12321
+# 1234321
+
+rows = 4
+for i in range(1,rows+1):
+
+    for j in range(1,i+1):
+        print(j,end="")
+
+    for j in range(i-1,0,-1):
+        print(j,end="")
+
+    print()
