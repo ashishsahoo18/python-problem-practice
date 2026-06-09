8b89580e32fd305d8f7e5803d3187e2f8aecac52
ashishsahoo18 <sahooashish673@gmail.com>
2026-06-09 11:31:54 -0500

    Update Day15.py

--- Day15.py
+++ Day15.py
@@ -3,3 +3,21 @@ if __name__ == '__main__':
     arr = map(int, input().split())
     
     print(sorted(set(arr))[-2])
+
+
+# 2.
+if __name__ == '__main__':
+    students = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name,score])
+    second_lowest = sorted(set(score for _, score in students))[1]
+    
+    names = []
+    for name,score in students:
+        if score == second_lowest:
+            names.append(name)
+    for name in sorted (names):
+        print(name)
+            
