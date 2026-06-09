b7b6e55653d8b29c66c859fbb0dc9fec0067b252
ashishsahoo18 <sahooashish673@gmail.com>
2026-06-08 20:42:52 -0500

    Create Day13.py

---
+++ Day13.py
@@ -0,0 +1,21 @@
+# 1.
+n = int(input())
+
+if n%2 !=0:
+    print("Weird")
+elif 2<=n<=5:
+    print("Not Weird")
+elif 6<=n<=20:
+    print("Weird")
+else:
+    print("Not Weird")
+
+# 2.
+a = int(input())
+b = int(input())
+c = a+b
+print(c)
+d = a-b
+print(d)
+e = a*b
+print(e)
