348c78d9391f7bf78bbc7612b388e6991f4b4f12
ashishsahoo18 <sahooashish673@gmail.com>
2026-07-10 18:23:50 -0500

    Update Day10.py

--- Day10.py
+++ Day10.py
@@ -9,3 +9,14 @@ for i in range(rows):
     spaces = rows -i -1
     stars = 2*i+1
     print(" " * spaces + "*" * stars)
+
+# 2.print this pattern
+#  *******
+#   *****
+#    ***
+#     *
+
+for i in range(rows):
+    spaces = i
+    stars = 2*(rows-i)-1
+    print(" "* spaces + "*"* stars)
