95df8ce7c1ec6fe2421b2039e0c349b209ea01c5
ashishsahoo18 <sahooashish673@gmail.com>
2026-07-21 19:18:22 -0500

    Update Day6.py

--- Day6.py
+++ Day6.py
@@ -1,7 +1,7 @@
 # 1.Print multiplication table of a number
 num = int(input("Enter a number"))
 for i in range(1,11):
-    print("num *",i,"=",num*i)
+    print(num,"*",i,"=",num*i)
 
 # 2.Print this pattern
 # *
