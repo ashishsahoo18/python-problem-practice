c1f01cad2c291f8565a7e8e1f0879118bde73f99
ashishsahoo18 <sahooashish673@gmail.com>
2026-07-22 20:42:08 -0500

    Update Day25.py

--- Day25.py
+++ Day25.py
@@ -6,3 +6,19 @@ print("Hello, World.")
 print(s)
 
 # Day 2
+i = 4
+d = 4.0
+s = 'HackerRank '
+d = 4.0
+s = 'HackerRank '
+
+i2 = int(input())
+d2 = float(input())
+s2 = input()
+
+print(i+i2)
+print(d+d2)
+print(s + s2)
+
+
+# Day 3
